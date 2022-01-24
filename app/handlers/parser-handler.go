4ef8b2e5162bfab1215299f24214e71cc3b687fd
Prabhansh Dhagat <pdhagat@deqode.com>
2022-01-24 15:39:38 +0530

    feat: add handler for word counts
    Add function to get occurences of all words in text. Add handler for word counts route

--- app/handlers/parser-handler.go
+++ app/handlers/parser-handler.go
@@ -3,7 +3,10 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"sort"
+	"strings"
 )
+
 type request struct {
 	Data string
 }
@@ -19,6 +22,48 @@ type successResponse struct {
 type wordOccurence struct {
 	Word  string `json:"word"`
 	Count int    `json:"count"`
+}
+
+// Function to handle word count request
+func WordCount(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var req request
+
+	// Decode request body
+	err := decoder.Decode(&req)
+	if err != nil {
+		returnBadRequest("Invalid request body", w)
+		return
+	}
+
+	if req.Data == "" {
+		returnBadRequest("Invalid request body", w)
+		return
+	}
+
+	// Get word counts
+	wordCount := getWordCount(req.Data)
+
+	returnSuccessfulResponse(wordCount, w)
+}
+
+// Function to get occurences of all words in text string
+func getWordCount(text string) (wordOccurences []wordOccurence) {
+	// Convert string text to slice array by separating through white spaces
+	words := strings.Fields(text)
+
+	// Map words to occurences in text
+	wordsMap := make(map[string]int)
+	for _, word := range words {
+		wordsMap[word]++
+	}
+
+	// Append word occurences tp struct
+	for word, count := range wordsMap {
+		wordOccurences = append(wordOccurences, wordOccurence{word, count})
+	}
+
+	return
 }
 // Function to return bad request to response
 func returnBadRequest(message string, w http.ResponseWriter) {
