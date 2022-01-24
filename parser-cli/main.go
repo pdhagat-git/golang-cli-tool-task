2b0a56e5efb0c368966a3dfdd44ac7fbbc90da04
Prabhansh Dhagat <pdhagat@deqode.com>
2022-01-24 16:34:43 +0530

    feat: add api call to get top 10 word occurrences

--- parser-cli/main.go
+++ parser-cli/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 
@@ -16,6 +19,39 @@ func init() {
 	if err != nil {
 		log.Fatal("Error: Unable to load env: ", err)
 	}
+}
+
+// Function to call API service to get top 10 most occured words in text string
+func getTopWords(text string) string {
+	// Create request body
+	requestBody := map[string]string{"data": text}
+	requestJSON, err := json.Marshal(requestBody)
+	if err != nil {
+		log.Fatal("Error: Unable to create request body: ", err)
+	}
+
+	// Call api to get top 10 most occurred words
+	response, err := http.Post(os.Getenv("API_URL"), "application/json", bytes.NewBuffer(requestJSON))
+	if err != nil {
+		log.Fatal("Error: Failed API call: ", err)
+	}
+
+	defer response.Body.Close()
+
+	// Read response body
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal("Error: Unable to read API response body: ", err)
+	}
+
+	// Prettify json response
+	var topWords bytes.Buffer
+	err = json.Indent(&topWords, responseBody, "", "  ")
+	if err != nil {
+		log.Fatal("Error: Unable to parse response body: ", err)
+	}
+
+	return topWords.String()
 }
 
 // Function to read file content at given path
@@ -44,8 +80,9 @@ func readFile(filePath string) string {
 
 func main() {
 
-	//Print contents of file
+	//Print word occurences in file
 	data := readFile(os.Getenv("FILE_PATH"))
-	log.Println(data)
+	topWords := getTopWords(data)
+	log.Println(topWords)
 
 }
